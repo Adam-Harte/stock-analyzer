aee786efc69bc354e126ce8d309bdff5867586fb
Adam-Harte <adamharte11@hotmail.co.uk>
2021-01-17 23:40:55 +0000

    StockSearch updated to mke pi calls on click and store the data then disable the search button for 1 minute to keep in line with the api restrictions

--- src/Components/StockSearch/StockSearch.tsx
+++ src/Components/StockSearch/StockSearch.tsx
@@ -1,35 +1,42 @@
 import React, { useState } from 'react';
 
+import { getResponseData } from '../../Api/alphaVantageApi';
 import { StockContext } from '../../Context/StockContext';
 
-const API_KEY = process.env.REACT_APP_ALPHAVANTAGE_API_KEY;
+import './StockSearch.css';
 
 export const StockSearch: React.FC = () => {
   const [ticker, setTicker] = useState<string>('');
+  const [searchDisabled, setSearchDisabled] = useState<boolean>(false);
   const [overview, setOverview] = useState<{ [key: string]: any } | null>(null);
   const [balanceSheet, setBalanceSheet] = useState<{ [key: string]: any } | null>(null);
   const [incomeStatement, setIncomeStatement] = useState<{ [key: string]: any } | null>(null);
   const [cashFlow, setCashFlow] = useState<{ [key: string]: any } | null>(null);
   const [earnings, setEarnings] = useState<{ [key: string]: any } | null>(null);
 
-  const getData = async(url: string, cb: (data: { [key: string]: any }) => void) => {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    cb(data);
-  };
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setTicker(e.currentTarget.value);
   };
 
-  const handleClick = (): void => {
-    getData(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${value}&apikey=${API_KEY}`, setOverview);
-    getData(`https://www.alphavantage.co/query?function==BALANCE_SHEET&symbol=${value}&apikey=${API_KEY}`, setBalanceSheet);
-    getData(`https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=${value}&apikey=${API_KEY}`, setIncomeStatement);
-    getData(`https://www.alphavantage.co/query?function=CASH_FLOW&symbol=${value}&apikey=${API_KEY}`, setCashFlow);
-    getData(`https://www.alphavantage.co/query?function=EARNINGS&symbol=${value}&apikey=${API_KEY}`, setEarnings);
+  const handleClick = async (): Promise<any> => {
+    setSearchDisabled(true);
+    const overviewData = await getResponseData('OVERVIEW', ticker.toUpperCase());
+    setOverview(overviewData);
+    const balanceSheetData = await getResponseData('BALANCE_SHEET', ticker.toUpperCase());
+    setBalanceSheet(balanceSheetData);
+    const incomeStatementData = await getResponseData('INCOME_STATEMENT', ticker.toUpperCase());
+    setIncomeStatement(incomeStatementData);
+    const cashFlowData = await getResponseData('CASH_FLOW', ticker.toUpperCase());
+    setCashFlow(cashFlowData);
+    const earningsData = await getResponseData('EARNINGS', ticker.toUpperCase());
+    setEarnings(earningsData);
+    setTimeout(() => {
+      setSearchDisabled(false);
+    }, 6000);
   };
+
+  console.log(overview);
+  console.log(cashFlow);
 
   return (
     <StockContext.Provider value={{
@@ -39,17 +46,25 @@ export const StockSearch: React.FC = () => {
       cashFlow,
       earnings,
     }}>
-      <div>
+      <div className="stock-search">
         <label htmlFor="stockInput">Get Stock key ratios</label>
         <input
+          className="stock-search__input"
           type="text"
           name="stockInput"
           id="stockInput"
-          prefix="Ticker"
+          prefix="Enter stock ticker symbol"
           onChange={handleChange}
           value={ticker}
         />
-        <button type="button" onClick={handleClick}>Get ratios</button>
+        <button
+          className="stock-search__button"
+          type="button"
+          onClick={handleClick}
+          disabled={searchDisabled}
+        >
+          Get ratios
+        </button>
       </div>
     </StockContext.Provider>
   );
